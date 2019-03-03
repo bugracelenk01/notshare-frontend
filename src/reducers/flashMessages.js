9cf65f0707e375418e4a3676f2492617b846f493
bugracelenk01 <bugracelenk01@gmail.com>
2019-03-03 15:19:34 +0300

    --logical-- added lodash, some changes on flashmessage reducer

--- src/reducers/flashMessages.js
+++ src/reducers/flashMessages.js
@@ -1,5 +1,7 @@
 import { ADD_FLASH_MESSAGE } from "actions/types";
 import shortid from "shortid";
+import findIndex from "lodash/findIndex";
+
 export default (state = [], action) => {
   switch (action.type) {
     case ADD_FLASH_MESSAGE:
@@ -11,6 +13,12 @@ export default (state = [], action) => {
           text: action.message.text
         }
       ];
+    case DELETE_FASH_MESSAGE:
+      const index = findIndex(state, { id: action.id });
+      if (index >= 0) {
+        return [...state.slice(0, index), ...state.slice(index + 1)];
+      }
+      return state;
     default:
       return state;
   }
