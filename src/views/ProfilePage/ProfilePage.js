26181c2b551d9b52b37ed4ea38bb948cbcb05a87
bugracelenk01 <bugracelenk01@gmail.com>
2019-03-09 19:23:15 +0300

    changed profile page, added notes, search by course name, profile desc, changed follow button to rate profile (all of them as template)

--- src/views/ProfilePage/ProfilePage.js
+++ src/views/ProfilePage/ProfilePage.js
@@ -7,11 +7,35 @@ import {
   Icon,
   Button,
   Label,
-  Image
+  Image,
+  Card,
+  Dropdown,
+  Menu
 } from "semantic-ui-react";
+import { stateOptions } from "../../common";
 
 class ProfilePage extends Component {
   render() {
+    const items = [
+      {
+        header: "Project Report - April",
+        description:
+          "Leverage agile frameworks to provide a robust synopsis for high level overviews.",
+        meta: "ROI: 30%"
+      },
+      {
+        header: "Project Report - May",
+        description:
+          "Bring to the table win-win survival strategies to ensure proactive domination.",
+        meta: "ROI: 34%"
+      },
+      {
+        header: "Project Report - June",
+        description:
+          "Capitalise on low hanging fruit to identify a ballpark value added activity to beta test.",
+        meta: "ROI: 27%"
+      }
+    ];
     return (
       <Container>
         <Grid centered columns={14}>
@@ -24,19 +48,36 @@ class ProfilePage extends Component {
                   src="https://react.semantic-ui.com/images/avatar/large/jenny.jpg"
                   circular
                 />
-                <Header
-                  as="h2"
-                  content="Account Settings"
-                  subheader="Manage your account settings and set email preferences"
-                />
+                <Header as="h2">
+                  <Header.Content>
+                    Altuğ Buğra Çelenk
+                    <Header.Subheader>
+                      <Icon name="location arrow" />
+                      Süleyman Demirel Üniversitesi
+                    </Header.Subheader>
+                    <Header.Subheader>
+                      <Icon name="history" />
+                      2.Sınıf
+                    </Header.Subheader>
+                    <Header.Subheader>
+                      <Icon name="user" />
+                      Bilgisayar Mühendisliği
+                    </Header.Subheader>
+                  </Header.Content>
+                </Header>
                 <Button
                   color="green"
                   content="Follow"
                   icon="plus"
                   size="large"
                   fluid
-                />
-                <br/>
+                >
+                  <Button.Content>
+                    <Icon name="thumbs up" />
+                    Rate Profile
+                  </Button.Content>
+                </Button>
+                <br />
                 <Button negative size="small" fluid>
                   Block or report this user
                 </Button>
@@ -44,7 +85,20 @@ class ProfilePage extends Component {
               <Grid.Column width={1} />
             </Grid>
           </Grid.Column>
-          <Grid.Column width={10}>hello world</Grid.Column>
+          <Grid.Column width={10}>
+            <Menu>
+              <Menu.Item>Search by Course Name</Menu.Item>
+              <Dropdown
+                placeholder="Course Names"
+                fluid
+                multiple
+                search
+                selection
+                options={stateOptions}
+              />
+            </Menu>
+            <Card.Group items={items} itemsPerRow="1"/>
+          </Grid.Column>
         </Grid>
       </Container>
     );
